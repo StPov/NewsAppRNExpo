9df1141df675736c4784954ad705f765a19a23fb
Stanislav <staspovolotskiy@gmail.com>
2020-05-24 19:06:33 +0300

    added AsyncStorage DB

--- src/screens/SavesScreen.js
+++ src/screens/SavesScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { FlatList, TextInput, View, StyleSheet } from "react-native";
+import {
+  FlatList,
+  TextInput,
+  View,
+  StyleSheet,
+  AsyncStorage,
+} from "react-native";
 import Article from "../services/models/Article";
 
 export default class App extends React.Component {
@@ -16,72 +22,88 @@ export default class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-    this.loadMoreData();
-  }
-
-  loadMoreData = (querry = "Sevastopol") => {
-    const key = "8c66ce1dfb9245cf9fe9be0a484d713e";
-    if (!this.state.fetching_from_server && !this.state.isListEnd) {
-      this.setState({ fetching_from_server: true, isListEnd: false }, () => {
-        fetch(
-          `https://newsapi.org/v2/everything?apiKey=${key}&page=${this.state.page}&q=${querry}`
-        )
-          .then((response) => response.json())
-          .then((responseJson) => {
-            if (responseJson.articles.length > 0) {
-              this.page = this.page + 1;
-              this.setState({
-                articles: [...this.state.articles, ...responseJson.articles],
-                fetching_from_server: false,
-                refreshing: false,
-              });
-            } else {
-              this.setState({
-                fetching_from_server: false,
-                isListEnd: true,
-              });
-            }
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      });
+  retrieveArticle = async () => {
+    try {
+      const value = await AsyncStorage.getItem("array");
+      if (value !== null) {
+        let parsedObj = JSON.parse(value);
+        this.setState({articles: parsedObj})
+        this.setState({refreshing: false})
+      }
+    } catch (error) {
+      // Error retrieving data
+      console.log("Retrieving error");
     }
   };
 
-  handleRefresh() {
-    this.setState({ articles: [], refreshing: true }, () =>
-      this.loadMoreData()
-    );
+  componentDidMount() {
+    this.retrieveArticle();
+    console.log(this.state.articles.length)
+    // this.loadMoreData();
   }
 
-  searchData(text) {
-    this.setState({ articles: [], refreshing: true, text: text }, () =>
-      setTimeout(() => {
-        this.loadMoreData(text);
-      }, 500)
-    );
-  }
+  //   loadMoreData = (querry = "Sevastopol") => {
+  //     const key = "8c66ce1dfb9245cf9fe9be0a484d713e";
+  //     if (!this.state.fetching_from_server && !this.state.isListEnd) {
+  //       this.setState({ fetching_from_server: true, isListEnd: false }, () => {
+  //         fetch(
+  //           `https://newsapi.org/v2/everything?apiKey=${key}&page=${this.state.page}&q=${querry}`
+  //         )
+  //           .then((response) => response.json())
+  //           .then((responseJson) => {
+  //             if (responseJson.articles.length > 0) {
+  //               this.page = this.page + 1;
+  //               this.setState({
+  //                 articles: [...this.state.articles, ...responseJson.articles],
+  //                 fetching_from_server: false,
+  //                 refreshing: false,
+  //               });
+  //             } else {
+  //               this.setState({
+  //                 fetching_from_server: false,
+  //                 isListEnd: true,
+  //               });
+  //             }
+  //           })
+  //           .catch((error) => {
+  //             console.error(error);
+  //           });
+  //       });
+  //     }
+  //   };
+
+    handleRefresh() {
+      this.setState({ articles: [], refreshing: true }, () =>
+        this.retrieveArticle()
+      );
+    }
+
+  //   searchData(text) {
+  //     this.setState({ articles: [], refreshing: true, text: text }, () =>
+  //       setTimeout(() => {
+  //         this.loadMoreData(text);
+  //       }, 500)
+  //     );
+  //   }
 
   render() {
     return (
       <View style={styles.MainContainer}>
-        <TextInput
+        {/* <TextInput
           style={styles.textInput}
           onChangeText={(text) => this.searchData(text)}
           value={this.state.text}
           underlineColorAndroid="transparent"
           placeholder="Search Here"
-        />
+        /> */}
         <FlatList
           data={this.state.articles}
           renderItem={({ item }) => <Article article={item} />}
           keyExtractor={(item) => item.url}
-          refreshing={this.state.refreshing}
-          onRefresh={this.handleRefresh.bind(this)}
-          onEndReached={this.loadMoreData}
-          onEndReachedThreshold={0.5}
+            refreshing={this.state.refreshing}
+            onRefresh={this.handleRefresh.bind(this)}
+          //   onEndReached={this.loadMoreData}
+          //   onEndReachedThreshold={0.5}
           style={{ marginTop: 10 }}
         />
       </View>
@@ -96,13 +118,13 @@ const styles = StyleSheet.create({
     margin: 5,
   },
 
-  textInput: {
-    textAlign: "left",
-    paddingLeft: 18,
-    height: 42,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 8,
-    backgroundColor: "#FFFF",
-  },
+  //   textInput: {
+  //     textAlign: "left",
+  //     paddingLeft: 18,
+  //     height: 42,
+  //     borderWidth: 1,
+  //     borderColor: "gray",
+  //     borderRadius: 8,
+  //     backgroundColor: "#FFFF",
+  //   },
 });
--- src/services/models/Article.js
+++ src/services/models/Article.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { View, Linking, TouchableOpacity, Alert } from "react-native";
+import {
+  View,
+  Linking,
+  TouchableOpacity,
+  Alert,
+  AsyncStorage,
+} from "react-native";
 import { Text, Card, Divider } from "react-native-elements";
 import moment from "moment";
 
 export default class Article extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      savedArticles: []
+    };
+  }
+  saveArticle = async (newsItem) => {
+    try {
+      this.setState({savedArticles: [...this.state.savedArticles, newsItem]})
+      // this.setState({savedArticles: this.state.savedArticles.push(newsItem)})
+      await AsyncStorage.setItem(
+        'array',
+        JSON.stringify(this.state.savedArticles)
+      );
+      console.log("Success")
+      // console.log(newsItem)
+    } catch (error) {
+      console.log("Failure")
+      console.log(error)
+    }
+  };
+
   render() {
     const {
       title,
@@ -29,14 +57,13 @@ export default class Article extends React.Component {
             [
               {
                 text: "Сохранить",
-                onPress: () =>
-                  console.log(
-                    "Новость сохранена. Просмотр ее возможен в 'избранном'"
-                  ),
+                onPress: () => {
+                    this.saveArticle(this.props.article)
+                },
               },
               {
                 text: "Отмена",
-                onPress: () => console.log("Нажата отмена"),
+                // onPress: () => console.log("Нажата отмена"),
                 style: "cancel",
               },
             ],
